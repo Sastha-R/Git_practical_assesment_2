06e7ff26e4627f4763fea588bd6a5ef2ce8f3109
Sastha <sastha00007@gmail.com>
2026-06-23 18:11:14 +0530

    fixed bug

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ document.getElementById("content").innerHTML =
     "<p>Feature added by Developer A</p>";
 
     document.getElementById("btn").textContent  = "Click";
+
+    document.getElementById("content").innerHTML =
+    "<p>Critical bug fixed in production</p>";
