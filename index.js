b5f1621779f6012bb6777be91363cc50d2ad935c
Sastha <sastha00007@gmail.com>
2026-06-23 18:01:35 +0530

    added a bug

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ document.getElementById("content").innerHTML =
 
     document.getElementById("content").innerHTML +=
     " <br><p>Feature A: Login UI added</p>";
+
+     document.getElementByClass("content").innerHTML +=
+    " <br><p>Feature A: added class instead of id</p>";
