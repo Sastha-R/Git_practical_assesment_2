20407ff5dbe663d172694f6215a7729025690f03
Sastha <sastha00007@gmail.com>
2026-06-23 17:20:21 +0530

    fixed button textcontent

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById("content").innerHTML =
     "<p>Feature added by Developer A</p>";
 
-    document.getElementById("btn").innerHTML = "Click";
+    document.getElementById("btn").textContent  = "Click";
